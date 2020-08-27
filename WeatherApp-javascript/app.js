d839b4a7e01c74dbefe30e82553ae402877057b7
Pavan Kumar Bairishetti <43448632+iambpawan@users.noreply.github.com>
2020-08-27 23:06:24 +0530

    Add files via upload

---
+++ WeatherApp-javascript/app.js
@@ -0,0 +1,48 @@
+const apiKey = 'f7a531c48a0d532cda136dfc251304b8';
+let latitude;
+let longitude;
+const notification = document.getElementsByClassName('notification')[0];
+let weather;
+
+getLocation();
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+}
+
+function kelvinToCelsius(temp) {
+    return temp - 273.15;
+}
+
+function onSuccess(position) {
+    console.log(position);
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    const weatherCall = fetch('https://api.openweathermap.org/data/2.5/weather?'
+                            + 'lat=' + latitude
+                            + '&lon=' + longitude
+                            + '&appid=' + apiKey);
+
+    weatherCall.then(response => response.json())
+            .then(weatherInfo => {
+                console.log(weatherInfo)
+                console.log(weatherInfo.weather[0].icon);
+                console.log(kelvinToCelsius(weatherInfo.main.temp).toFixed(1));
+                console.log(weatherInfo.weather[0].main);
+                console.log(weatherInfo.name);
+            });
+}
+
+function onError(error) {
+    console.error('No no no ', error);
+    // 1. take message and put it in a p
+    const p = document.createElement('p');
+    p.innerHTML = error.message;
+    // 2. display: block (notification div)
+    notification.style.display = 'block';
+    // 3. append p inside notification
+    notification.appendChild(p);
+}
+
